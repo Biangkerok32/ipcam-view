41371f9e7a0fb1ff41627650c5bc93bd68242acf
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2020-04-17 10:27:25 +0700

    Update Mjpeg.java

--- mjpeg-view/src/main/java/com/github/niqdev/mjpeg/Mjpeg.java
+++ mjpeg-view/src/main/java/com/github/niqdev/mjpeg/Mjpeg.java
@@ -1,6 +1,7 @@
 package com.github.niqdev.mjpeg;
 
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 
